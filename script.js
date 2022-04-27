70d392953c3f9900ff619a62fb5f0959c948a1ac
Paulo Adeva <paulo.adeva@gmail.com>
2022-04-27 23:58:32 +1000

    need to change card look

--- script.js
+++ script.js
@@ -25,15 +25,34 @@ function Movie(title, director, genre, runtime, year) {
 Movie.prototype.createMovie = function() {
     console.log("This is the movie" + this.title + this.director + this.genre + this.runtime + this.year);
 
+    const newContainer = document.createElement('div');
+    newContainer.style.border = '5px solid black';
+    movieContainer.appendChild(newContainer);
+
     const newTitle = document.createElement('span');
     newTitle.innerText = newMovie.title;
     newTitle.style.border = '1px solid blue';
-    movieContainer.appendChild(newTitle);
+    newContainer.appendChild(newTitle);
 
     const newDirector = document.createElement('span');
     newDirector.innerText = newMovie.director;
     newDirector.style.border = '1px solid red';
-    movieContainer.appendChild(newDirector)
+    newContainer.appendChild(newDirector);
+
+    const newGenre = document.createElement('span');
+    newGenre.innerText = newMovie.genre;
+    newGenre.style.border = '1px solid green';
+    newContainer.appendChild(newGenre);
+
+    const newRuntime = document.createElement('span');
+    newRuntime.innerText = newMovie.runtime;
+    newRuntime.style.border = '1px solid purple';
+    newContainer.appendChild(newRuntime);
+
+    const newYear = document.createElement('span');
+    newYear.innerText = newMovie.year;
+    newYear.style.border = '1px solid pink';
+    newContainer.appendChild(newYear);
 
 
     modal.style.display = 'none';
